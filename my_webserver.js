4e40b2d431a3e22ec677502fa925ba185487a9b9
comrade <aitashish173@gmail.com>
2014-05-12 21:06:58 +0530

    simple http server

---
+++ my_webserver.js
@@ -0,0 +1,9 @@
+// importing http module
+var http = require('http');
+
+http.createServer(function (req, res) { 
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(8080);
+
+console.log('Server is running');
